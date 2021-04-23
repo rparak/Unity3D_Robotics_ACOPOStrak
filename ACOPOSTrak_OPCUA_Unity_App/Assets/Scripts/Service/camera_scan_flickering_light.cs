c5791d4213a256e2f395ff2d2899bb21e9cb7989
Roman Parak <59449320+rparak@users.noreply.github.com>
2021-04-23 10:25:41 +0100

    Update camera_scan_flickering_light.cs

--- ACOPOSTrak_OPCUA_Unity_App/Assets/Scripts/Service/camera_scan_flickering_light.cs
+++ ACOPOSTrak_OPCUA_Unity_App/Assets/Scripts/Service/camera_scan_flickering_light.cs
@@ -1,6 +1,6 @@
 ﻿/****************************************************************************
 MIT License
-Copyright(c) 2020 Roman Parak
+Copyright(c) 2021 Roman Parak
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
